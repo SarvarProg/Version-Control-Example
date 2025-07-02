b6c91aeed3661957328831976161f2c39f61464b
unknown <connorkenway5566@gmail.com>
2025-07-03 00:32:35 +0500

    done with all things

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,10 +4,14 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 4.0f;
+    [SerializeField]
+    private GameObject _laserPrefab;
 
     private Player _player;
     private Animator _anim;
     private AudioSource _audioSource;
+    private float _fireRate = 3.0f;
+    private float _canFire = -1;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,15 +32,32 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+        if(Time.time > _canFire )
+        {
+            _fireRate = Random.Range(3f, 7f);
+            _canFire = Time.time + _fireRate;
+            GameObject enemyLaser = Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+            Laser[] lasers = enemyLaser.GetComponentsInChildren<Laser>();
+
+            for (int i=0; i < lasers.Length; i++)
+            {
+                lasers[i].AssignEnemyLaser();
+            }
+        }
+    }
+
+    void CalculateMovement()
+    {
+
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
 
-        if(transform.position.y < -5f)
+        if (transform.position.y < -5f)
         {
             float randomX = Random.Range(-8f, 8f);
             transform.position = new Vector3(randomX, 7, 0);
         }
     }
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
@@ -66,6 +87,7 @@ public class Enemy : MonoBehaviour
             _anim.SetTrigger("EnemyDeath");
             _speed = 0;
             _audioSource.Play();
+            Destroy(GetComponent<Collider2D>());
             Destroy(this.gameObject, 2.8f);
         }
     }
--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -4,25 +4,69 @@ public class Laser : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 8.0f;
+    private bool _isEnemyLaser = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (_isEnemyLaser == false)
+        {
+            MoveUp();
+        }
+        else
+        {
+            MoveDown();
+        }
+    }
+
+    void MoveUp()
+    {
         transform.Translate(Vector3.up * _speed * Time.deltaTime);
 
-        if(transform.position.y > 6f)
+        if (transform.position.y > 6f)
         {
             if (transform.parent != null)
             {
                 Destroy(transform.parent.gameObject);
             }
             Destroy(this.gameObject, 3f);
+        }
+    }
+
+    void MoveDown()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y < -6f)
+        {
+            if (transform.parent != null)
+            {
+                Destroy(transform.parent.gameObject);
+            }
+            Destroy(this.gameObject, 3f);
+        }
+    }
+
+    public void AssignEnemyLaser()
+    {
+        _isEnemyLaser = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player" && _isEnemyLaser == true)
+        {
+            Player player = other.GetComponent<Player>();
+            if (player != null)
+            {
+                player.Damage();
+            }
         }
     }
 }
